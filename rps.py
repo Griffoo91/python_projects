25e9c8fb752e4180213ea694733b901815406a47
Griffoo91 <omboggorised91@gmail.com>
2024-03-22 13:55:35 +0300

    2nd commit: inclusion of while loop to exit the game

--- rps.py
+++ rps.py
@@ -7,33 +7,43 @@ class RPS(Enum):
     PAPER = 2
     SCISSOR = 3
 
-#input for player choice
-print("")
-playerchoice = input("Please enter...\n1 for ROCK\n2 for PAPER, or\n3 for SCISSOR\n\n")
-player = int(playerchoice)
+playagain = True
+while playagain:
 
-#checking the user input
-if player < 1 or player > 3:
-    sys.exit("The number should be either 1,2 or 3.Try again later!")
+    #input for player choice
+    print("")
+    playerchoice = input("Please enter...\n1 for ROCK\n2 for PAPER, or\n3 for SCISSOR\n\n")
+    player = int(playerchoice)
 
-# computer choice randomly generated
-computerchoice = random.choice("123")
-computer = int(computerchoice)
+    #checking the user input
+    if player < 1 or player > 3:
+        sys.exit("The number should be either 1,2 or 3.Try again later!")
 
-#displaying the choices of both computer and player
-print("")
-print("You chose " + str(RPS(player)).replace("RPS.", "") + ".")
-print("Computer chose " + str(RPS(computer)).replace("RPS.", "") + ".")
-print("")
+    # computer choice randomly generated
+    computerchoice = random.choice("123")
+    computer = int(computerchoice)
 
-#logic of the game
-if player == 1 and computer == 3:
-    print("you win!")
-elif player == 2 and computer == 1:
-    print("You win!")
-elif player == 3 and computer == 2:
-    print("You win!")
-elif player == computer:
-    print("Tie game!!")
-else:
-    print("Computer win")
+    #displaying the choices of both computer and player
+    print("")
+    print("You chose " + str(RPS(player)).replace("RPS.", "") + ".")
+    print("Computer chose " + str(RPS(computer)).replace("RPS.", "") + ".")
+    print("")
+
+    #logic of the game
+    if player == 1 and computer == 3:
+        print("you win!")
+    elif player == 2 and computer == 1:
+        print("You win!")
+    elif player == 3 and computer == 2:
+        print("You win!")
+    elif player == computer:
+        print("Tie game!!")
+    else:
+        print("Computer win")
+    
+    playagain = input("\nPlay again? \n Y for Yes \n Q for Quit\n\n")
+    if playagain.lower() == "y":
+        continue
+    else:
+        print("Thank you for playing.")
+        sys.exit("Bye!")
